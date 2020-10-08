1aa594acecf0b1a8f50359377cdce4624befa974
dianaavila2018 <45690382+dianaavila2018@users.noreply.github.com>
2020-10-07 21:35:13 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,76 +1,60 @@
 import requests
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          RegexHandler, ConversationHandler, CallbackQueryHandler)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler, 
+                            CallbackQueryHandler)
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
-
 
 STATE1 = 1
 STATE2 = 2
 
-def welcome(update, context):
+def inicio(update, context):
     try:
-        username = update.message.from_user.username
         firstName = update.message.from_user.first_name
-        lastName = update.message.from_user.last_name
         message = 'Olá, ' + firstName + '!'
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    except Exception as e:
-        print(str(e))
+    except Exception as error:
+        print(str(error))
 
-
-def feedback(update, context):
+def resposta(update, context):
     try:
-        message = 'Por favor, digite um feedback para o nosso tutorial:'
+        message = '''Que informação deseja ?\n
+        1 - Cuidados com felinos\n
+        2 - Alimentação\n
+        3 - Castração\n
+        4 - Vacinação\n'''
+        # envia a mesagem para o usuario e guarda a resposta dele 
         update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
         return STATE1
-    except Exception as e:
-        print(str(e))
+    except Exception as error:
+        print(str(error))
 
 
-def inputFeedback(update, context):
-    feedback = update.message.text
-    print(feedback)
-    if len(feedback) < 10:
-        message = """Seu feedback foi muito curtinho... 
-                        \nInforma mais pra gente, por favor?"""
+def resposta1(update, context):
+    resposta = update.message.text
+    print(resposta)
+    if (resposta=='1' or resposta=='cuidados' or resposta=='cuidar'):
+        message =''' Ok.. vamos falar sobre cuidados com seus peludinhos\n'''
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-        return STATE1
+        return STATE2
+    elif (resposta=='2' or resposta=='alimentacao' or resposta=='alimentação' or resposta=='alimento'
+                or resposta=='ração' or resposta=='racao' or resposta=='comida' ):
+        message = '''  Ok.. vamos falar sobre a alimentação dos seus peludinhos\n'''
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE2
+    elif (resposta=='3' or resposta=='castração' or resposta=='castracao' or resposta=='castrar'
+                or resposta=='castrado'):
+        message = '''  Ok.. vamos falar sobre castração\n'''
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE2
     else:
-        message = "Muito obrigada pelo seu feedback!"
         context.bot.send_message(chat_id=update.effective_chat.id, text=message)
         return ConversationHandler.END
 
 
-def inputFeedback2(update, context):
-    feedback = update.message.text
-    message = "Muito obrigada pelo seu feedback!"
+def resposta2(update, context):
+    resposta = update.message.text
+    message = "Muito obrigada pela visita!"
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     return ConversationHandler.END
-
-
-# https://getemoji.com/
-def askForNota(update, context):
-    try:
-        question = 'Qual nota você dá para o tutorial?'
-        keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("👎 1", callback_data='1'),
-              InlineKeyboardButton("2", callback_data='2'),
-              InlineKeyboardButton("🤔 3", callback_data='3'),
-              InlineKeyboardButton("4", callback_data='4'),
-              InlineKeyboardButton("👍 5", callback_data='5')]])
-        update.message.reply_text(question, reply_markup=keyboard)
-    except Exception as e:
-        print(str(e))
-
-
-def getNota(update, context):
-    try:
-        query = update.callback_query
-        print(str(query.data))
-        message = 'Obrigada pela sua nota: ' + str(query.data) 
-        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    except Exception as e:
-        print(str(e))
 
 
 def cancel(update, context):
@@ -79,31 +63,25 @@ def cancel(update, context):
 
 def main():
     try:
-        # token = os.getenv('TELEGRAM_BOT_TOKEN', None)
-        token = 'cole_aqui_o_token_de_acesso_do_seu_bot'
+        token = '1300724353:AAGRE-9-bMyX3RLAbCRn7-dPha2NZF1EoHs'
         updater = Updater(token=token, use_context=True)
 
-        updater.dispatcher.add_handler(CommandHandler('start', welcome))
+        updater.dispatcher.add_handler(CommandHandler('start', inicio))
 
         conversation_handler = ConversationHandler(
-            entry_points=[CommandHandler('feedback', feedback)],
+            entry_points=[CommandHandler('feedback', resposta)],
             states={
-                STATE1: [MessageHandler(Filters.text, inputFeedback)],
-                STATE2: [MessageHandler(Filters.text, inputFeedback2)]
+                STATE1: [MessageHandler(Filters.text, resposta1)],
+                STATE2: [MessageHandler(Filters.text, resposta2)]
             },
+            # caso o usuário desista da conversa
             fallbacks=[CommandHandler('cancel', cancel)])
         updater.dispatcher.add_handler(conversation_handler)
-
-        updater.dispatcher.add_handler(CommandHandler('nota', askForNota))
-        updater.dispatcher.add_handler(CallbackQueryHandler(getNota))
-
         print("Updater no ar: " + str(updater))
         updater.start_polling()
         updater.idle()
-    except Exception as e:
-        print(str(e))
-
+    except Exception as error:
+        print(str(error))
 
 if __name__ == "__main__":
     main()
-
